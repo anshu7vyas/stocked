13e0791c37adc8e69a7617d1b2e15d519fbbb93b
av-7 <amanshulvyas@gmail.com>
2015-10-07 23:38:00 -0700

    Replaced ActionBar with ToolBar in all activities

--- Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/AddItemActivity.java
+++ Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/AddItemActivity.java
@@ -1,11 +1,23 @@
 package com.anshulvyas.csc780.grocerymanagr;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by av7 on 9/24/15.
  */
 public class AddItemActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Set a ToolBar to replace the ActionBar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Add Item");
+
+    }
 }
--- Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/HomeActivity.java
+++ Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/HomeActivity.java
@@ -1,25 +1,38 @@
 package com.anshulvyas.csc780.grocerymanagr;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         FloatingActionButton fab = (FloatingActionButton)  findViewById(R.id.FAB);
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, AddItemActivity.class);
                 startActivity(intent);
+                //Snackbar.make(v, "Hello Snackbar", Snackbar.LENGTH_LONG).show();
             }
         });
     }
