3b8db1fdc72202fbbbdaa0ca58e98380b8106444
av-7 <amanshulvyas@gmail.com>
2015-12-16 15:44:56 -0800

    added notification receiver.

--- Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/Adapters/TimelineAdapter.java
+++ Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/Adapters/TimelineAdapter.java
@@ -1,26 +1,7 @@
 package com.anshulvyas.csc780.grocerymanagr.Adapters;
 
-import android.content.Context;
-import android.widget.ArrayAdapter;
-
-import com.anshulvyas.csc780.grocerymanagr.Product;
-
-import java.util.List;
-
 /**
- * Created by av7 on 12/3/15.
+ * Created by av7 on 12/13/15.
  */
-public class TimelineAdapter extends ArrayAdapter<Product> {
-    private List<Product> timelineList;
-    Context myContext;
-    int mResouce;
-
-    public TimelineAdapter(Context context, int resource, List<Product> objects) {
-        super(context, resource, objects);
-        this.myContext = context;
-        this.mResouce = resource;
-        this.timelineList = objects;
-    }
-
-
+public class TimelineAdapter {
 }
---
+++ Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/NotificationReceiver.java
@@ -0,0 +1,7 @@
+package com.anshulvyas.csc780.grocerymanagr;
+
+/**
+ * Created by av7 on 12/13/15.
+ */
+public class NotificationReceiver {
+}
