92ec21fec0de72dc98059a011c0d943794a42cce
av-7 <amanshulvyas@gmail.com>
2015-12-18 18:11:11 -0800

    Fixed: Home ListView update after notificationDialog, Updated README.md, made UI improvements

--- Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/StockedApp.java
+++ Source/app/src/main/java/com/anshulvyas/csc780/grocerymanagr/StockedApp.java
@@ -8,7 +8,7 @@ import net.danlew.android.joda.JodaTimeAndroid;
 /**
  * Class represents the whole application. Primarily used for instantiating 3rd party library - JodaTimeAndroid
  */
-public class FooDeDApp extends Application {
+public class StockedApp extends Application {
 
     /**
      * Called when the activity is starting
